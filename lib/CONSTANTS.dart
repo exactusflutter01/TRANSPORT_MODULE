400962eacd34c0f77d6140d3507121b7947c1f9b
Hrithin1010 <Hrithinak@gmail.com>
2025-03-20 10:28:46 +0530

    20-03

--- lib/CONSTANTS.dart
+++ lib/CONSTANTS.dart
@@ -1,1 +1,17 @@
-const String baseUrl = "http://scm06.exactuscloud.com:4028/api";
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const String baseUrl = "http://scm06.exactuscloud.com:4016/api";
+
+final TextStyle appbarTextStyle = GoogleFonts.mitr(
+  fontWeight: FontWeight.w800,
+  color: const Color.fromARGB(255, 24, 88, 28),
+  fontSize: 27,
+);
+
+final TextStyle mainscreenTextStyle = GoogleFonts.bebasNeue(
+  fontWeight: FontWeight.bold,
+  // color: const Color.fromARGB(255, 42, 128, 48),
+  color: Colors.black,
+  fontSize: 25,
+);
